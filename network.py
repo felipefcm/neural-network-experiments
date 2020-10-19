99b67fdcca55492af22fd7ba7468fd13cd1ec506
Felipe Martins <ffcm@outlook.com>
2020-10-19 23:21:57 +1300

    Added biases and SGD (WIP)

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 
+import random
 import numpy as np
 
 class NeuralNetwork:
@@ -9,25 +10,45 @@ class NeuralNetwork:
 		self.neuronsPerLayer = neuronsPerLayer
 		
 		self.weights = []
+
 		for current, last in zip(neuronsPerLayer[1:], neuronsPerLayer[:-1]):
 			self.weights.append(np.random.randn(current, last))
 
+		self.biases = [ np.random.randn(y, 1) for y in neuronsPerLayer[1:] ]
+		
 	def feedForward(self, x):
 		
-		for w in self.weights:
-			x = relu(np.dot(w, x))
+		for b, w in zip(self.biases, self.weights):
+			x = relu(np.dot(w, x) + b)
 		
 		return x
+
+	def sgd(self, trainingData, epochs, miniBatchSize, lr, test=None):
+
+		n = len(trainingData)
+		for e in range(epochs):
+			
+			random.shuffle(trainingData)
+			miniBatches = [
+				trainingData[k : k + miniBatchSize]
+				for k in range(0, n, miniBatchSize)
+			]
+
+			for miniBatch in miniBatches:
+				self.adjustWeightsForMiniBatch(miniBatch, lr)
+
 
 	def backprop(self, x, expected):
 		
 		weightGradients = [ np.zeros(w.shape) for w in self.weights ]
+		biasGradients = [ np.zeros(b.shape) for b in self.biases ]
+
 		zs = []
 		activation = np.array(x)
 		activations = [ np.array(x) ]
 
-		for w in self.weights:
-			z = np.dot(w, activation)
+		for b, w in zip(self.biases, self.weights):
+			z = np.dot(w, activation) + b
 			zs.append(z)
 			activation = relu(z)
 			activations.append(activation)
@@ -36,22 +57,39 @@ class NeuralNetwork:
 			reluDerivative(zs[-1])
 
 		weightGradients[-1] = np.dot(delta, activations[-2].transpose())
+		biasGradients[-1] = delta
 
 		for layer in range(2, self.numLayers):
+			
 			z = zs[-layer]
 			d = reluDerivative(z)
 			delta = np.dot(self.weights[-layer + 1].transpose(), delta) * d
+			
 			weightGradients[-layer] = np.dot(
 				delta, activations[-layer - 1].transpose()
 			)
 
-		return weightGradients
+			biasGradients[-layer] = delta
+
+		return (biasGradients, weightGradients)
 
 	def adjustWeights(self, lr, weightGradients):
 		self.weights = [
 			w - lr * nw 
 			for w, nw in zip(self.weights, weightGradients)
 		]
+
+	def adjustWeightsForMiniBatch(self, miniBatch, lr):
+
+		weightGradients = [ np.zero(w.shape) for w in self.weights ]
+		biasGradients = [ np.zero(b.shape) for b in self.biases ]
+
+		for x, expected in miniBatch:
+			deltaBias, deltaWeight = self.backprop(x, expected)
+			weightGradients = [ nw + dnw for nw, dnw in zip(weightGradients, deltaWeight) ]
+			biasGradients = [ nb + dnb for nb, dnb in zip(biasGradients, deltaBias) ]
+
+		# self.weights = [w - (lr / len(miniBatch)) ]
 
 	def costDerivative(self, output, expected):
 		return output - expected
